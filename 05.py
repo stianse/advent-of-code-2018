3c8fff6efb89dd7bf287313117a398d7a9f3d39d
Stian Selnes <stian@pexip.com>
2018-12-05 20:30:31 +0100

    Day 5: Use stack and lookup dict
    Use a lookup dict to find if characters match is much neater.
    
    Use a stack instead of that awful slicing and copying.

--- 05.py
+++ 05.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
+import string
 
 with open('05.txt') as f:
-    inputs = f.read().replace('\n', '')
+    inputs = f.read().strip()
 
-A = [ord(c) for c in inputs]
-D = ord('a') - ord('A')
+A = inputs
+T = {}
+
+for x, y in zip(string.ascii_lowercase, string.ascii_uppercase):
+    T[x] = y
+    T[y] = x
 
 def react(a):
-    i = 0
-    while i < len(a) - 1:
-        if abs(a[i] - a[i+1]) == D:
-            a = a[:i] + a[i+2:]
-            if i > 0:
-                i -= 1
+    stack = []
+    for c in a:
+        if stack and T[c] == stack[-1]:
+            stack.pop()
         else:
-            i += 1
-    return a
+            stack.append(c)
+    return stack
 
 # Part 1
 print(len(react(A)))
 
 # Part 2
-best_len = None
-for x in range(ord('A'), ord('Z') + 1):
-    a = [c for c in A if c not in (x, x + D)]
+best_len = len(A) + 1
+for x in string.ascii_lowercase:
+    a = [c for c in A if c.lower() != x]
     r = react(a)
-    if best_len is None or len(r) < best_len:
+    if len(r) < best_len:
         best_len = len(r)
 print(best_len)
